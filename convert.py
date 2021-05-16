00b6e9f45d98f4cf996082bea199c7a8478b6576
Sinpei Araki <speedsolve@gmail.com>
2021-05-16 12:43:21 +0900

    サイト用にconvert改修

--- convert.py
+++ convert.py
@@ -9,3 +9,12 @@ request.add_header('Content-Type', 'text/plain')
 f = urllib.request.urlopen(request, data)
 with open('scj_regulations.html', 'bw') as file:
     file.write(f.read())
+
+with open('scj_regulations.html') as file:
+    data_lines = file.read()
+
+data_lines = data_lines.replace('h1', 'h4')
+data_lines = data_lines.replace('h2', 'h5')
+
+with open('scj_regulations.html', 'w') as file:
+    file.write(data_lines)
